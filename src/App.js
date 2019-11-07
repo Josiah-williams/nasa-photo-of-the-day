a38c29d032a7724751bda0e118988644ec7853d6
josiah <josiahdamiwilliams@gmail.com>
2019-11-07 16:07:02 +0100

    Finish part 1 partially

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import axios from "axios";
 import "./App.css";
+import CreatePhoto from "./Components/photoComponent";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=fzliRPzGtv8laLOSxUmx3BaFza3Hdc6nhogBMxba")
+    .then( (response)=> {
+      //Handle sucess
+      console.log(response.data);
+      setData(response.data);
+
+    })
+    .catch(function (error) {
+     // handle error
+      console.log(error);
+    });
+
+  }, []);
+  console.log(data);
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-    </div>
+      <p>test1</p>
+      <CreatePhoto obj={data}/>
+      </div>
   );
 }
 
